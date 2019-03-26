78d84a332b499684a6395be50a2c0df8739630ea
Martin <martinch14@gmail.com>
2019-03-26 20:54:56 -0300

    remove folder ejercicio1/out

--- ejercicio1/out/libs/lpc_open/lpc_startup/src/vendor_interrupt.d
+++
@@ -1,2 +0,0 @@
-mis_proyectos/ejercicio1/out/libs/lpc_open/lpc_startup/src/vendor_interrupt.o: \
- libs/lpc_open/lpc_startup/src/vendor_interrupt.c
--- ejercicio1/out/libs/sys_newlib/src/system.d
+++
@@ -1,2 +0,0 @@
-mis_proyectos/ejercicio1/out/libs/sys_newlib//src/system.o: \
- libs/sys_newlib//src/system.c
