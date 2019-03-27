7bf968823bfc4d22653610ccca2dcbd3a7cd52ca
Martin <martinch14@gmail.com>
2019-03-26 21:02:30 -0300

    remove folder ejercicio2/out

--- ejercicio2/out/libs/cmsis_core/src/armv7m_startup.d
+++
@@ -1,2 +0,0 @@
-mis_proyectos/ejercicio2/out/libs/cmsis_core//src/armv7m_startup.o: \
- libs/cmsis_core//src/armv7m_startup.c
--- ejercicio2/out/libs/sys_newlib/src/system.d
+++
@@ -1,2 +0,0 @@
-mis_proyectos/ejercicio2/out/libs/sys_newlib//src/system.o: \
- libs/sys_newlib//src/system.c
